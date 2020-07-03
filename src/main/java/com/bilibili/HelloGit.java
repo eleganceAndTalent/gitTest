b794ee2e81e4966029c3a4c913d849babb8d111c
yzj <873003866@qq.com>
2020-07-03 12:22:47 +0800

    second commit

--- src/main/java/com/bilibili/HelloGit.java
+++ src/main/java/com/bilibili/HelloGit.java
@@ -3,5 +3,6 @@ package com.bilibili;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello 1");
     }
 }
---
+++ src/main/java/com/bilibili/HelloGit2.java
@@ -0,0 +1,9 @@
+package com.bilibili;
+
+import java.sql.SQLOutput;
+
+public class HelloGit2 {
+    public static void main(String[] args) {
+        System.out.println("hello 22");
+    }
+}
